d7370bead3744d0af8e52a73b13afdd902919290
Melkizedeki <mmagota2@gmail.com>
2025-02-06 15:28:32 +0300

    commit Cheking the input field (TextField)

---
+++ lib/components/button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.onPress,
+      required this.buttonText,
+      required this.buttonColor});
+  final VoidCallback onPress;
+  final Color buttonColor;
+  final String buttonText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: MaterialButton(
+          onPressed: onPress,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            buttonText,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatting_app/components/button.dart';
 import 'package:chatting_app/screens/login_screen.dart';
 import 'package:chatting_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
@@ -103,41 +104,19 @@ class _WelcomeScreenState extends State<WelcomeScreen>
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            CustomButton(
+              onPress: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+              buttonText: "Login",
+              buttonColor: Colors.lightBlueAccent,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            CustomButton(
+              onPress: () {
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+              buttonText: "Register",
+              buttonColor: Colors.blueAccent,
             ),
           ],
         ),
